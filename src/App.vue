993360755f6956a122a54232f6e99e016878fd42
Nick Sonner <nicksonner@gmail.com>
2018-01-29 17:44:28 +1100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  },
+
+  data() {
+    return {
+      todos: [
+        {
+          title: 'Todo A',
+          project: 'Project A',
+          complete: false,
+        },
+        {
+          title: 'Todo B',
+          project: 'Project B',
+          complete: false,
+        },
+        {
+          title: 'Todo C',
+          project: 'Project C',
+          complete: false,
+        },
+      ]
+    }
+  }
+
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
